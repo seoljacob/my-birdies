3aa64172d4106f67199a2cbbc408d5f48c463224
Jacob Seol <jacobseol96@gmail.com>
2026-01-27 21:47:36 -0800

    Hide pii

--- reserve.py
+++ reserve.py
@@ -1,13 +1,13 @@
 from playwright.sync_api import sync_playwright
-import time
+import os
 
-def sign_in(p):
+def sign_in(p, e, ph):
     try:
         p.goto("https://mybirdies.ca")
         p.get_by_role("link", name="Sign In").click()
-        p.get_by_placeholder("you@example.com").fill("jacobseol96@gmail.com")
+        p.get_by_placeholder("you@example.com").fill(e)
         p.get_by_role("button", name="Continue").click()
-        p.get_by_placeholder("7781234567").fill("2365136930")
+        p.get_by_placeholder("7781234567").fill(ph)
         p.get_by_role("button", name="Sign In").click()
     except Error as e:
         print(f"{type(e).__name__}: {e}")
@@ -15,10 +15,11 @@ def sign_in(p):
 def register(p, days):
     for day in days:
         try:
-            print(f"Registering for {day}")
+            print(f"Registering for {day}...")
             #p.get_by_text(day).click()
             #p.get_by_role("button", name="Register Now").click()
-            #p.get_by_text("Birdie Buddies").click()
+            #p.get_by_text("Birdie Buddies".click()
+            print("Registered for {day}!")
         except Error as e:
             print(f"{type(e).__name__}: {e}")
 
@@ -26,7 +27,7 @@ def main():
     with sync_playwright() as p:
         browser = p.chromium.launch()
         page = browser.new_page()
-        sign_in(page)
+        sign_in(page, os.getenv("EMAIL"), os.getenv("PHONE_NUMBER"))
         register(page, ["Monday", "Wednesday"])
         browser.close()        
 
