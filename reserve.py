a3e0ee72e89d1ebd8d56f9a9eda37fbd0a7b1968
Jacob Seol <jacobseol96@gmail.com>
2026-01-27 21:33:29 -0800

    Add reserve logic

--- reserve.py
+++ reserve.py
@@ -1,24 +1,36 @@
 from playwright.sync_api import sync_playwright
 import time
 
+def sign_in(p):
+    try:
+        p.goto("https://mybirdies.ca")
+        p.get_by_role("link", name="Sign In").click()
+        p.get_by_placeholder("you@example.com").fill("jacobseol96@gmail.com")
+        p.get_by_role("button", name="Continue").click()
+        p.get_by_placeholder("7781234567").fill("2365136930")
+        p.get_by_role("button", name="Sign In").click()
+    except Error as e:
+        print(f"{type(e).__name__}: {e}")
+
+def register(p, days):
+    for day in days:
+        try:
+            print(f"Registering for {day}")
+            #p.get_by_text(day).click()
+            #p.get_by_role("button", name="Register Now").click()
+            #p.get_by_text("Birdie Buddies").click()
+        except Error as e:
+            print(f"{type(e).__name__}: {e}")
+
 def main():
-    print("Hello, World!")
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        sign_in(page)
+        register(page, ["Monday", "Wednesday"])
+        browser.close()        
 
 if __name__=="__main__":
     main()
 
-#with sync_playwright() as p:
-#    browser = p.chromium.launch(headless=False, slow_mo=100)
-#    page = browser.new_page()
-#    page.goto("https://mybirdies.ca")
-#    print(page.title())
-#    sign_in = page.get_by_role("link", name="Sign In").click()
-#    page.get_by_placeholder("you@example.com").fill("jacobseol96@gmail.com")
-#    page.get_by_role("button", name="Continue").click()
-#    page.get_by_placeholder("7781234567").fill("2365136930")
-#    page.get_by_role("button", name="Sign In").click()
-#    page.get_by_text("Monday").click()
-#    page.get_by_role("button", name="Register Now").click()
-#    time.sleep(3000) 
-#    browser.close()
-#
+
